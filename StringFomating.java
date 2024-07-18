c43860d0a488303cc24d7b0163ff24066974afe2
LahiruMudith <lahimudith@gmail.com>
2024-07-18 14:41:07 +0530

    String Formating

---
+++ StringFomating.java
@@ -0,0 +1,9 @@
+package waterLevelProject;
+
+public class StringFomating {
+        public static void main(String[] args) {
+            String str = "Ada nam harima amaru dawasak";
+            String s = String.format("%100s", str);
+            System.out.println(s);
+    }
+}
